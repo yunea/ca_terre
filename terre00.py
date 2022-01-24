2b4142e62a3f2f3b2888827e416ea1c347152228
yunea <code.experience01@gmail.com>
2022-01-24 17:18:12 +0100

    exo 1 fini

---
+++ terre00.py
@@ -0,0 +1,9 @@
+# L'alphabet
+# exercice terre00.py
+
+alphabet_list=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+alphabet_text=""
+for a in alphabet_list :
+    alphabet_text=str(alphabet_text)+str(a)
+alphabet_text=str(alphabet_text)+'\n'
+print(alphabet_text)
