9394d4c51fa13746dd04071c5446aa4cf085ed4b
yunea <code.experience01@gmail.com>
2022-01-24 17:32:14 +0100

    fait

--- terre00.py
+++ terre00.py
@@ -1,5 +1,5 @@
 # L'alphabet
-# exercice terre00.py
+# Exercice 1 : terre00.py
 
 alphabet_list=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 alphabet_text=""
